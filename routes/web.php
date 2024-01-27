ab33ee10c99d0eb1b8fe10de249bc9aa96967849
Atif Ibrahim <dedezigners@gmail.com>
2024-01-27 10:23:26 +0500

    fixin

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\FrontendController;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use App\Http\Controllers\Auth\AuthenticatedSessionController;
-use App\Http\Controllers\Auth\ConfirmablePasswordController;
-use App\Http\Controllers\Auth\EmailVerificationNotificationController;
-use App\Http\Controllers\Auth\EmailVerificationPromptController;
-use App\Http\Controllers\Auth\NewPasswordController;
-use App\Http\Controllers\Auth\PasswordController;
-use App\Http\Controllers\Auth\PasswordResetLinkController;
-use App\Http\Controllers\Auth\RegisteredUserController;
-use App\Http\Controllers\Auth\VerifyEmailController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -43,4 +32,8 @@ Route::middleware('guest')->group(function () {
 Route::middleware('auth')->group(function () {
     Route::any('logout', [AuthController::class, 'destroy'])->name('logout');
     Route::get('dashboard', [FrontendController::class, 'dashboard'])->name('dashboard');
+    
+    // Admin Routes
+    Route::redirect('admin', 'admin/dashboard');
+    Route::get('admin/dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
 });
