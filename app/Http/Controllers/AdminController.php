b17bb6e8883173e7c66ebeb20042a9b425b686c8
Atif Ibrahim <dedezigners@gmail.com>
2024-02-12 18:07:55 +0500

    dashboard updates

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog\Category;
+use App\Models\Blog\Post;
+use App\Models\Blog\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -9,6 +13,11 @@ class AdminController extends Controller
 {
     public function dashboard()
     {
-        return Inertia::render("Admin/Dashboard");
+        return Inertia::render("Admin/Dashboard", [
+            'user-count' => User::count(),
+            'post-count' => Post::count(),
+            'category-count' => Category::count(),
+            'tag-count' => Tag::count(),
+        ]);
     }
 }
--- resources/js/Admin/Dashboard.vue
+++ resources/js/Admin/Dashboard.vue
@@ -1,6 +1,43 @@
 <template>
     <admin-layout :title="title" :breadcrumbs="breadcrumbs">
-        Hello World!
+        <div class="row">
+            <div class="col-12 col-md-6 col-xl-3">
+                <div class="de-card de-card__flush">
+                    <font-awesome-icon class="bg-dark" icon="user-group" />
+                    <div class="de-card__flush--info">
+                        <h4>Users</h4>
+                        <h2>{{ userCount ?? 0 }}</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3">
+                <div class="de-card de-card__flush">
+                    <font-awesome-icon class="bg-primary" icon="newspaper" />
+                    <div class="de-card__flush--info">
+                        <h4>Posts</h4>
+                        <h2>{{ postCount ?? 0 }}</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3">
+                <div class="de-card de-card__flush">
+                    <font-awesome-icon class="bg-info" icon="layer-group" />
+                    <div class="de-card__flush--info">
+                        <h4>Categories</h4>
+                        <h2>{{ categoryCount ?? 0 }}</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 col-xl-3">
+                <div class="de-card de-card__flush">
+                    <font-awesome-icon class="bg-warning" icon="tags" />
+                    <div class="de-card__flush--info">
+                        <h4>Tags</h4>
+                        <h2>{{ tagCount ?? 0 }}</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
     </admin-layout>
 </template>
 
@@ -12,6 +49,10 @@ export default {
     props: {
         title: String,
         breadcrumbs: Array as PropType<String[]>,
+        userCount: Number,
+        categoryCount: Number,
+        tagCount: Number,
+        postCount: Number,
     },
     setup: (props) => {}
 }
